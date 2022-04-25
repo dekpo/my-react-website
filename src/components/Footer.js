844c944d739a0fa00159b9b97e2458a3073d74d6
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2022-04-25 11:36:04 +0200

    Created header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return(
+        <footer className="text-center bg-dark text-white m-2">
+            <p>DWWM - &copy; CCI</p>
+        </footer>
+    )
+}
+export default Footer
