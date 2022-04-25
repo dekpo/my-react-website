d904dfb7baa8c7890f6265589ab6fce30f2bcc49
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2022-04-25 12:32:54 +0200

    Added react-router-dom and used it in the App file to declare routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/porfolio" element={ <Portfolio /> } />
+          <Route path="/contact" element={ <Contact /> } />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
