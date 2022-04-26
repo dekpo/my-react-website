e5b315100c664b339c2473ee430d7fe1df42cafc
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2022-04-26 11:45:01 +0200

    Added a Card component for the Portfolio loop

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import Card from '../components/Card';
 
 const Portfolio = () => {
     // on déclare la variable joke ET sa fonction/méthode setJoke
@@ -47,18 +48,12 @@ const Portfolio = () => {
                 </div>
                 <div className="row">
                     {
-                        gallery.map((pic) => {
+                        gallery.map((item) => {
+                            let source = `https://picsum.photos/id/${item.id}/320/240`;
+                            let title = `Picture by ${item.author}`;
+                            let id = item.id;
                             return (
-                                <div key={pic.id} className="col-12 col-md-4">
-                                    <div className="card">
-                                        <img className="img-fluid mb-2 card-img-top" src={pic.download_url} alt={pic.author} />
-                                            <div className="card-body">
-                                                <h5 className="card-title">Card title</h5>
-                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                             <Card key={id} source={source} title={title} />
                         )
                     })
                     }
